96cbc2f1fd2b8c9d6391bd0c9f21a2bac5421d74
Zanatti <fabio.zanatti@newconsoftware.com.br>
2020-11-18 14:52:47 -0300

    ajuste

--- src/Containers/ComprarAcao/action.js
+++ src/Containers/ComprarAcao/action.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3500";
+const BASE_URL = "https://api-investimento.herokuapp.com/";
 
 export async function buscarAcao(tiker) {
     debugger
